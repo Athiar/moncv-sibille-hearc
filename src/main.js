b5a73c053f2fb03bc2b34478f695f811455dc047
Nicolas Sibille <nicolas.sibillerhe-arc.ch>
2017-11-03 11:17:42 +0100

    Ajout du code JS qui modifie les progressbar en chart

--- src/main.js
+++ src/main.js
@@ -2,7 +2,67 @@ import 'bootstrap.native';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './main.css';
 import 'jquery-smooth-scroll';
+import Chart from 'chart.js';
 
 $(document).ready(() => {
     $('a').smoothScroll();
+
+    // $('.progress-bar').each((i, progressbar) => {
+        // $('body').prepend('<h1> valeur : ' + $(progressbar).attr('aria-valuenow') + '</h1>');
+    // });
+
+    Chart.pluginService.register({
+        beforeDraw: function (chart) {
+            var width = chart.chart.width;
+            var height = chart.chart.height;
+            var ctx = chart.chart.ctx;
+            ctx.restore();
+            var fontSize = (height / 90).toFixed(2);
+            ctx.font = fontSize + 'em sans-serif';
+            ctx.textBaseline = 'middle';
+            var text = chart.chart.data.datasets[0].data[0] + '%';
+            var textX = Math.round((width - ctx.measureText(text).width) / 2) + 5;
+            var textY = (height / 2) + 17;
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+        }
+    });
+
+    $('.progress-bar').each((i, progressbar) => {
+        var ctx = $(progressbar).parent()[0].getContext('2d');
+        window.myDoughnut = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [
+                        $(progressbar).attr('aria-valuenow'), (100 - $(progressbar).attr('aria-valuenow'))
+                    ],
+                    backgroundColor: ['#428bca', 'rgba(0, 0, 0, 0.0)'],
+                    hoverBackgroundColor: ['#63a8e2', 'rgba(0, 0, 0, 0.0)'],
+                    borderColor: ['#052d4f', 'rgba(0, 0, 0, 0.0)']
+                }]
+            },
+            options: {
+                cutoutPercentage: 75,
+                responsive: false, // A améliorer si possible
+                tooltips: {
+                    enabled: false
+                },
+                title: {
+                    display: true,
+                    text: $(progressbar).text(),
+                    fontSize: '20'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                },
+                layout: {
+                    padding: {
+                        bottom: 20
+                    }
+                }
+            }
+        });
+    });
 });
