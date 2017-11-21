284e2b519c13ae7413dd80d0438b81c8b1b38d8f
Nicolas Sibille <nicolas.sibillerhe-arc.ch>
2017-11-21 15:46:43 +0100

    124ce85

---
+++ static/js/app.993d6db5c7c8760d217b.js
@@ -0,0 +1,368 @@
+webpackJsonp([1],{
+
+/***/ "L0+G":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "NHnr":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function($) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_bootstrap_native__ = __webpack_require__("XxhF");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_bootstrap_native___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_bootstrap_native__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_bootstrap_css_only_css_bootstrap_min_css__ = __webpack_require__("L0+G");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_bootstrap_css_only_css_bootstrap_min_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_bootstrap_css_only_css_bootstrap_min_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__main_css__ = __webpack_require__("Q00M");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__main_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__main_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_jquery_smooth_scroll__ = __webpack_require__("X9XS");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_jquery_smooth_scroll___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_jquery_smooth_scroll__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_chart_js__ = __webpack_require__("WaEV");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_chart_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_chart_js__);
+
+
+
+
+
+
+$(document).ready(function () {
+    $('a').smoothScroll();
+    __WEBPACK_IMPORTED_MODULE_4_chart_js___default.a.pluginService.register({
+        beforeDraw: function beforeDraw(chart) {
+            var width = chart.chart.width;
+            var height = chart.chart.height;
+            var ctx = chart.chart.ctx;
+            ctx.restore();
+            var fontSize = (height / 90).toFixed(2);
+            ctx.font = fontSize + 'em sans-serif';
+            ctx.textBaseline = 'middle';
+            var text = chart.chart.data.datasets[0].data[0] + '%';
+            var textX = Math.round((width - ctx.measureText(text).width) / 2) + 3;
+            var textY = height / 2 + 17;
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+        }
+    });
+
+    $('.progress-bar').each(function (i, progressbar) {
+        var ctx = $(progressbar).parent()[0].getContext('2d');
+        window.myDoughnut = new __WEBPACK_IMPORTED_MODULE_4_chart_js___default.a(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [$(progressbar).attr('aria-valuenow'), 100 - $(progressbar).attr('aria-valuenow')],
+                    backgroundColor: ['#428bca', 'rgba(0, 0, 0, 0.0)'],
+                    hoverBackgroundColor: ['#63a8e2', 'rgba(0, 0, 0, 0.0)'],
+                    borderColor: ['#052d4f', 'rgba(0, 0, 0, 0.0)']
+                }]
+            },
+            options: {
+                cutoutPercentage: 75,
+                responsive: true,
+                tooltips: {
+                    enabled: false
+                },
+                title: {
+                    display: true,
+                    text: $(progressbar).text(),
+                    fontSize: '20'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                },
+                layout: {
+                    padding: {
+                        bottom: 20
+                    }
+                }
+            }
+        });
+    });
+});
+
+window.sendMessage = function () {
+    if ($('#inputName').val().length !== 0 && $('#inputEmail').val().length !== 0 && $('#inputEntreprise').val().length !== 0 && $('#message').val().length !== 0) {
+        var _require = __webpack_require__("2vHO"),
+            TelegramClient = _require.TelegramClient;
+
+        var clientTG = TelegramClient.connect('320992887:AAHOQFbzqE1TMcGWDU2x7357tHkkR_d8tKs');
+        clientTG.sendMessage('157513870', 'Message reçu de ' + $('#inputName').val() + ' (' + $('#inputEmail').val() + ') de l\'entreprise ' + $('#inputEntreprise').val() + ' : ' + $('#message').val()).then(function () {
+            window.alert('Votre message a bien été envoyé.');
+            $('#message').val('');
+        }).catch(function (err) {
+            window.alert('Erreur ' + err + '. Désolé votre message n\'a pas pu être envoyé.');
+        });
+    } else {
+        window.alert('Erreur, veuillez compléter tous les champs.');
+    }
+};
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("7t+N")))
+
+/***/ }),
+
+/***/ "Q00M":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "uslO":
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./af": "3CJN",
+	"./af.js": "3CJN",
+	"./ar": "3MVc",
+	"./ar-dz": "tkWw",
+	"./ar-dz.js": "tkWw",
+	"./ar-kw": "j8cJ",
+	"./ar-kw.js": "j8cJ",
+	"./ar-ly": "wPpW",
+	"./ar-ly.js": "wPpW",
+	"./ar-ma": "dURR",
+	"./ar-ma.js": "dURR",
+	"./ar-sa": "7OnE",
+	"./ar-sa.js": "7OnE",
+	"./ar-tn": "BEem",
+	"./ar-tn.js": "BEem",
+	"./ar.js": "3MVc",
+	"./az": "eHwN",
+	"./az.js": "eHwN",
+	"./be": "3hfc",
+	"./be.js": "3hfc",
+	"./bg": "lOED",
+	"./bg.js": "lOED",
+	"./bn": "aM0x",
+	"./bn.js": "aM0x",
+	"./bo": "w2Hs",
+	"./bo.js": "w2Hs",
+	"./br": "OSsP",
+	"./br.js": "OSsP",
+	"./bs": "aqvp",
+	"./bs.js": "aqvp",
+	"./ca": "wIgY",
+	"./ca.js": "wIgY",
+	"./cs": "ssxj",
+	"./cs.js": "ssxj",
+	"./cv": "N3vo",
+	"./cv.js": "N3vo",
+	"./cy": "ZFGz",
+	"./cy.js": "ZFGz",
+	"./da": "YBA/",
+	"./da.js": "YBA/",
+	"./de": "DOkx",
+	"./de-at": "8v14",
+	"./de-at.js": "8v14",
+	"./de-ch": "Frex",
+	"./de-ch.js": "Frex",
+	"./de.js": "DOkx",
+	"./dv": "rIuo",
+	"./dv.js": "rIuo",
+	"./el": "CFqe",
+	"./el.js": "CFqe",
+	"./en-au": "Sjoy",
+	"./en-au.js": "Sjoy",
+	"./en-ca": "Tqun",
+	"./en-ca.js": "Tqun",
+	"./en-gb": "hPuz",
+	"./en-gb.js": "hPuz",
+	"./en-ie": "ALEw",
+	"./en-ie.js": "ALEw",
+	"./en-nz": "dyB6",
+	"./en-nz.js": "dyB6",
+	"./eo": "Nd3h",
+	"./eo.js": "Nd3h",
+	"./es": "LT9G",
+	"./es-do": "7MHZ",
+	"./es-do.js": "7MHZ",
+	"./es.js": "LT9G",
+	"./et": "XlWM",
+	"./et.js": "XlWM",
+	"./eu": "sqLM",
+	"./eu.js": "sqLM",
+	"./fa": "2pmY",
+	"./fa.js": "2pmY",
+	"./fi": "nS2h",
+	"./fi.js": "nS2h",
+	"./fo": "OVPi",
+	"./fo.js": "OVPi",
+	"./fr": "tzHd",
+	"./fr-ca": "bXQP",
+	"./fr-ca.js": "bXQP",
+	"./fr-ch": "VK9h",
+	"./fr-ch.js": "VK9h",
+	"./fr.js": "tzHd",
+	"./fy": "g7KF",
+	"./fy.js": "g7KF",
+	"./gd": "nLOz",
+	"./gd.js": "nLOz",
+	"./gl": "FuaP",
+	"./gl.js": "FuaP",
+	"./gom-latn": "+27R",
+	"./gom-latn.js": "+27R",
+	"./he": "Nzt2",
+	"./he.js": "Nzt2",
+	"./hi": "ETHv",
+	"./hi.js": "ETHv",
+	"./hr": "V4qH",
+	"./hr.js": "V4qH",
+	"./hu": "xne+",
+	"./hu.js": "xne+",
+	"./hy-am": "GrS7",
+	"./hy-am.js": "GrS7",
+	"./id": "yRTJ",
+	"./id.js": "yRTJ",
+	"./is": "upln",
+	"./is.js": "upln",
+	"./it": "FKXc",
+	"./it.js": "FKXc",
+	"./ja": "ORgI",
+	"./ja.js": "ORgI",
+	"./jv": "JwiF",
+	"./jv.js": "JwiF",
+	"./ka": "RnJI",
+	"./ka.js": "RnJI",
+	"./kk": "j+vx",
+	"./kk.js": "j+vx",
+	"./km": "5j66",
+	"./km.js": "5j66",
+	"./kn": "gEQe",
+	"./kn.js": "gEQe",
+	"./ko": "eBB/",
+	"./ko.js": "eBB/",
+	"./ky": "6cf8",
+	"./ky.js": "6cf8",
+	"./lb": "z3hR",
+	"./lb.js": "z3hR",
+	"./lo": "nE8X",
+	"./lo.js": "nE8X",
+	"./lt": "/6P1",
+	"./lt.js": "/6P1",
+	"./lv": "jxEH",
+	"./lv.js": "jxEH",
+	"./me": "svD2",
+	"./me.js": "svD2",
+	"./mi": "gEU3",
+	"./mi.js": "gEU3",
+	"./mk": "Ab7C",
+	"./mk.js": "Ab7C",
+	"./ml": "oo1B",
+	"./ml.js": "oo1B",
+	"./mr": "5vPg",
+	"./mr.js": "5vPg",
+	"./ms": "ooba",
+	"./ms-my": "G++c",
+	"./ms-my.js": "G++c",
+	"./ms.js": "ooba",
+	"./my": "F+2e",
+	"./my.js": "F+2e",
+	"./nb": "FlzV",
+	"./nb.js": "FlzV",
+	"./ne": "/mhn",
+	"./ne.js": "/mhn",
+	"./nl": "3K28",
+	"./nl-be": "Bp2f",
+	"./nl-be.js": "Bp2f",
+	"./nl.js": "3K28",
+	"./nn": "C7av",
+	"./nn.js": "C7av",
+	"./pa-in": "pfs9",
+	"./pa-in.js": "pfs9",
+	"./pl": "7LV+",
+	"./pl.js": "7LV+",
+	"./pt": "ZoSI",
+	"./pt-br": "AoDM",
+	"./pt-br.js": "AoDM",
+	"./pt.js": "ZoSI",
+	"./ro": "wT5f",
+	"./ro.js": "wT5f",
+	"./ru": "ulq9",
+	"./ru.js": "ulq9",
+	"./sd": "fW1y",
+	"./sd.js": "fW1y",
+	"./se": "5Omq",
+	"./se.js": "5Omq",
+	"./si": "Lgqo",
+	"./si.js": "Lgqo",
+	"./sk": "OUMt",
+	"./sk.js": "OUMt",
+	"./sl": "2s1U",
+	"./sl.js": "2s1U",
+	"./sq": "V0td",
+	"./sq.js": "V0td",
+	"./sr": "f4W3",
+	"./sr-cyrl": "c1x4",
+	"./sr-cyrl.js": "c1x4",
+	"./sr.js": "f4W3",
+	"./ss": "7Q8x",
+	"./ss.js": "7Q8x",
+	"./sv": "Fpqq",
+	"./sv.js": "Fpqq",
+	"./sw": "DSXN",
+	"./sw.js": "DSXN",
+	"./ta": "+7/x",
+	"./ta.js": "+7/x",
+	"./te": "Nlnz",
+	"./te.js": "Nlnz",
+	"./tet": "gUgh",
+	"./tet.js": "gUgh",
+	"./th": "XzD+",
+	"./th.js": "XzD+",
+	"./tl-ph": "3LKG",
+	"./tl-ph.js": "3LKG",
+	"./tlh": "m7yE",
+	"./tlh.js": "m7yE",
+	"./tr": "k+5o",
+	"./tr.js": "k+5o",
+	"./tzl": "iNtv",
+	"./tzl.js": "iNtv",
+	"./tzm": "FRPF",
+	"./tzm-latn": "krPU",
+	"./tzm-latn.js": "krPU",
+	"./tzm.js": "FRPF",
+	"./uk": "ntHu",
+	"./uk.js": "ntHu",
+	"./ur": "uSe8",
+	"./ur.js": "uSe8",
+	"./uz": "XU1s",
+	"./uz-latn": "/bsm",
+	"./uz-latn.js": "/bsm",
+	"./uz.js": "XU1s",
+	"./vi": "0X8Q",
+	"./vi.js": "0X8Q",
+	"./x-pseudo": "e/KL",
+	"./x-pseudo.js": "e/KL",
+	"./yo": "YXlc",
+	"./yo.js": "YXlc",
+	"./zh-cn": "Vz2w",
+	"./zh-cn.js": "Vz2w",
+	"./zh-hk": "ZUyn",
+	"./zh-hk.js": "ZUyn",
+	"./zh-tw": "BbgG",
+	"./zh-tw.js": "BbgG"
+};
+function webpackContext(req) {
+	return __webpack_require__(webpackContextResolve(req));
+};
+function webpackContextResolve(req) {
+	var id = map[req];
+	if(!(id + 1)) // check for number or string
+		throw new Error("Cannot find module '" + req + "'.");
+	return id;
+};
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "uslO";
+
+/***/ })
+
+},["NHnr"]);
+//# sourceMappingURL=app.993d6db5c7c8760d217b.js.map
